8bd7888d65afba3a4a77b2309d17bd571e66edbc
Ivan Skokovskyi <ivanskokovskyi@gmail.com>
2020-11-23 16:14:21 +0000

    Add (plugin): ZeroToHero

---
+++ custom/plugins/ZeroToHero/src/Resources/app/storefront/src/example-plugin/example-plugin.plugin.js
@@ -0,0 +1,13 @@
+// src/Resources/app/storefront/src/example-plugin/example-plugin.plugin.js
+
+import Plugin from 'src/plugin-system/plugin.class';
+
+export default class ExamplePlugin extends Plugin {
+  init() {
+        window.onscroll = function() {
+            if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight) {
+                alert('seems like there\'s nothing more to see here.');
+            }
+        };
+    }
+}
---
+++ custom/plugins/ZeroToHero/src/ZeroToHero.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace ZeroToHero;
+
+use Shopware\Core\Framework\Plugin;
+
+class ZeroToHero extends Plugin
+{
+}
