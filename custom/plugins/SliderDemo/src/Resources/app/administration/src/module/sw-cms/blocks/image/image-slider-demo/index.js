6dc71221b6988379f3e6bb36682c86abd44cfdae
Ivan Skokovskyi <ivanskokovskyi@gmail.com>
2020-11-24 21:58:56 +0000

    Test (adding cms blocks):

---
+++ custom/plugins/SliderDemo/src/Resources/app/administration/src/module/sw-cms/blocks/image/image-slider-demo/index.js
@@ -0,0 +1,20 @@
+import './component';
+import './preview';
+
+Shopware.Service('cmsService').registerCmsBlock({
+    name: 'image-slider-demo',
+    label: 'sw-cms.blocks.image.imageSliderDemo.label',
+    category: 'image',
+    component: 'sw-cms-block-image-slider-demo',
+    previewComponent: 'sw-cms-preview-image-slider-demo',
+    defaultConfig: {
+        marginBottom: '20px',
+        marginTop: '20px',
+        marginLeft: '20px',
+        marginRight: '20px',
+        sizingMode: 'boxed'
+    },
+    slots: {
+        imageSlider: 'image-slider-demo'
+    }
+});
---
+++ custom/plugins/SliderDemo/src/Resources/app/administration/src/module/sw-cms/blocks/image/image-slider-demo/preview/index.js
@@ -0,0 +1,8 @@
+import template from './sw-cms-preview-image-slider-demo.html.twig';
+import './sw-cms-preview-image-slider-demo.scss';
+
+const { Component } = Shopware;
+
+Component.register('sw-cms-preview-image-slider-demo.scss', {
+    template
+});
